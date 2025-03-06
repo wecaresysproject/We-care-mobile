753a3dffdf0d74383ef7a725507148aa4fce2ed7
Aya Nady <ayanady22193@gmail.com>
2025-03-06 22:46:58 +0200

    Implment delete analysis by id

--- lib/core/global/SharedWidgets/details_view_app_bar.dart
+++ lib/core/global/SharedWidgets/details_view_app_bar.dart
@@ -3,13 +3,14 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:we_care/core/global/Helpers/functions.dart';
 import 'package:we_care/core/global/SharedWidgets/custom_app_back_arrow.dart';
 import 'package:we_care/core/global/theming/app_text_styles.dart';
+import 'package:we_care/core/global/theming/color_manager.dart';
 
 class DetailsViewAppBar extends StatelessWidget {
-  const DetailsViewAppBar({
-    super.key,
-    required this.title,
-  });
+  const DetailsViewAppBar(
+      {super.key, required this.title, this.editFunction, this.deleteFunction});
   final String title;
+  final Function()? editFunction;
+  final Function()? deleteFunction;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,47 @@ class DetailsViewAppBar extends StatelessWidget {
             style: AppTextStyles.font20blackWeight600.copyWith(fontSize: 21.sp),
           ),
         ),
-        horizontalSpacing(24.h)
+        Row(
+          children: [
+            InkWell(
+              onTap: editFunction,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColorsManager.mainDarkBlue,
+                  borderRadius: BorderRadius.circular(16.r),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 8.h),
+                child: Row(children: [
+                  Image.asset(
+                    'assets/images/edit.png',
+                    width: 15.w,
+                    height: 15.h,
+                  ),
+                  Text('تعديل', style: AppTextStyles.font14whiteWeight600),
+                ]),
+              ),
+            ),
+            horizontalSpacing(8.w),
+            InkWell(
+              onTap: deleteFunction,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColorsManager.warningColor,
+                  borderRadius: BorderRadius.circular(16.r),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 8.h),
+                child: Row(children: [
+                  Image.asset(
+                    'assets/images/delete.png',
+                    width: 15.w,
+                    height: 15.h,
+                  ),
+                  Text('حذف', style: AppTextStyles.font14whiteWeight600),
+                ]),
+              ),
+            )
+          ],
+        )
       ],
     );
   }
--- lib/core/networking/auth_service.g.dart
+++ lib/core/networking/auth_service.g.dart
@@ -2,227 +2,76 @@
 
 part of 'auth_service.dart';
 
-// **************************************************************************
+// ***************************************************************************
 // RetrofitGenerator
-// **************************************************************************
-
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+// ***************************************************************************
 
 class _AuthApiServices implements AuthApiServices {
-  _AuthApiServices(this._dio, {this.baseUrl, this.errorLogger}) {
-    baseUrl ??= 'http://147.93.57.70/api/v1/auth';
+  _AuthApiServices(this._dio, {this.baseUrl}) {
+    baseUrl ??= AuthApiConstants.baseUrl;
   }
 
   final Dio _dio;
 
   String? baseUrl;
 
-  final ParseErrorLogger? errorLogger;
-
   @override
   Future<SignUpResponseModel> signup(
-    SignUpRequestBodyModel signupRequestBody,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(signupRequestBody.toJson());
-    final _options = _setStreamType<SignUpResponseModel>(
-      Options(method: 'POST', headers: _headers, extra: _extra)
-          .compose(
-            _dio.options,
-            '/register',
-            queryParameters: queryParameters,
-            data: _data,
-          )
-          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+      SignUpRequestBodyModel signupRequestBody) async {
+    final response = await _dio.post(
+      AuthApiConstants.signUpEndPoint,
+      data: signupRequestBody.toJson(),
     );
-    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
-    late SignUpResponseModel _value;
-    try {
-      _value = SignUpResponseModel.fromJson(_result.data!);
-    } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options);
-      rethrow;
-    }
-    return _value;
+    return SignUpResponseModel.fromJson(response.data);
   }
 
   @override
   Future<VerifyOtpResponseModel> verifyOtp(
-    VerifyOtpRequestBodyModel verifyOtpRequestBody,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(verifyOtpRequestBody.toJson());
-    final _options = _setStreamType<VerifyOtpResponseModel>(
-      Options(method: 'POST', headers: _headers, extra: _extra)
-          .compose(
-            _dio.options,
-            '/verify-otp',
-            queryParameters: queryParameters,
-            data: _data,
-          )
-          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+      VerifyOtpRequestBodyModel verifyOtpRequestBody) async {
+    final response = await _dio.post(
+      AuthApiConstants.verifyOtpEndPoint,
+      data: verifyOtpRequestBody.toJson(),
     );
-    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
-    late VerifyOtpResponseModel _value;
-    try {
-      _value = VerifyOtpResponseModel.fromJson(_result.data!);
-    } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options);
-      rethrow;
-    }
-    return _value;
+    return VerifyOtpResponseModel.fromJson(response.data);
   }
 
   @override
   Future<LoginResponseModel> login(
-    LoginRequestBodyModel loginRequestBody,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(loginRequestBody.toJson());
-    final _options = _setStreamType<LoginResponseModel>(
-      Options(method: 'POST', headers: _headers, extra: _extra)
-          .compose(
-            _dio.options,
-            '/login',
-            queryParameters: queryParameters,
-            data: _data,
-          )
-          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+      LoginRequestBodyModel loginRequestBody) async {
+    final response = await _dio.post(
+      AuthApiConstants.loginEndPoint,
+      data: loginRequestBody.toJson(),
     );
-    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
-    late LoginResponseModel _value;
-    try {
-      _value = LoginResponseModel.fromJson(_result.data!);
-    } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options);
-      rethrow;
-    }
-    return _value;
+    return LoginResponseModel.fromJson(response.data);
   }
 
   @override
   Future<ResendOtpResponseModel> resendOtp(
-    ResendOtpRequestBody resendOtpRequestBody,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(resendOtpRequestBody.toJson());
-    final _options = _setStreamType<ResendOtpResponseModel>(
-      Options(method: 'POST', headers: _headers, extra: _extra)
-          .compose(
-            _dio.options,
-            '/resend-otp',
-            queryParameters: queryParameters,
-            data: _data,
-          )
-          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+      ResendOtpRequestBody resendOtpRequestBody) async {
+    final response = await _dio.post(
+      AuthApiConstants.resendOtpEndPoint,
+      data: resendOtpRequestBody.toJson(),
     );
-    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
-    late ResendOtpResponseModel _value;
-    try {
-      _value = ResendOtpResponseModel.fromJson(_result.data!);
-    } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options);
-      rethrow;
-    }
-    return _value;
+    return ResendOtpResponseModel.fromJson(response.data);
   }
 
   @override
   Future<CreateNewPasswordResponseModel> createNewPassword(
-    CreateNewPasswordRequestBody createNewPasswordRequestBody,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(createNewPasswordRequestBody.toJson());
-    final _options = _setStreamType<CreateNewPasswordResponseModel>(
-      Options(method: 'PUT', headers: _headers, extra: _extra)
-          .compose(
-            _dio.options,
-            '/change-password',
-            queryParameters: queryParameters,
-            data: _data,
-          )
-          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+      CreateNewPasswordRequestBody createNewPasswordRequestBody) async {
+    final response = await _dio.put(
+      AuthApiConstants.createNewPasswordEndPoint,
+      data: createNewPasswordRequestBody.toJson(),
     );
-    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
-    late CreateNewPasswordResponseModel _value;
-    try {
-      _value = CreateNewPasswordResponseModel.fromJson(_result.data!);
-    } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options);
-      rethrow;
-    }
-    return _value;
+    return CreateNewPasswordResponseModel.fromJson(response.data);
   }
 
   @override
   Future<ForgetPasswordResponseModel> forgetPassword(
-    ForgetPasswordRequestBodyModel forgetPasswordRequestBody,
-  ) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(forgetPasswordRequestBody.toJson());
-    final _options = _setStreamType<ForgetPasswordResponseModel>(
-      Options(method: 'POST', headers: _headers, extra: _extra)
-          .compose(
-            _dio.options,
-            '/forgot-password',
-            queryParameters: queryParameters,
-            data: _data,
-          )
-          .copyWith(baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl)),
+      ForgetPasswordRequestBodyModel forgetPasswordRequestBody) async {
+    final response = await _dio.post(
+      AuthApiConstants.forgotPasswordEndPoint,
+      data: forgetPasswordRequestBody.toJson(),
     );
-    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
-    late ForgetPasswordResponseModel _value;
-    try {
-      _value = ForgetPasswordResponseModel.fromJson(_result.data!);
-    } on Object catch (e, s) {
-      errorLogger?.logError(e, s, _options);
-      rethrow;
-    }
-    return _value;
-  }
-
-  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
-    if (T != dynamic &&
-        !(requestOptions.responseType == ResponseType.bytes ||
-            requestOptions.responseType == ResponseType.stream)) {
-      if (T == String) {
-        requestOptions.responseType = ResponseType.plain;
-      } else {
-        requestOptions.responseType = ResponseType.json;
-      }
-    }
-    return requestOptions;
-  }
-
-  String _combineBaseUrls(String dioBaseUrl, String? baseUrl) {
-    if (baseUrl == null || baseUrl.trim().isEmpty) {
-      return dioBaseUrl;
-    }
-
-    final url = Uri.parse(baseUrl);
-
-    if (url.isAbsolute) {
-      return url.toString();
-    }
-
-    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+    return ForgetPasswordResponseModel.fromJson(response.data);
   }
 }
