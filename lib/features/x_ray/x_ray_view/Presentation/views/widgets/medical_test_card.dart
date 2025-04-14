4d9ab53eaf57d63f2c24f0ed6792808f3c7253d7
Aya Nady <ayanady22193@gmail.com>
2025-04-14 20:20:28 +0200

    Updates .gitignore and enhances various UI components for better display and functionality

--- lib/features/x_ray/x_ray_view/Presentation/views/widgets/medical_test_card.dart
+++ lib/features/x_ray/x_ray_view/Presentation/views/widgets/medical_test_card.dart
@@ -37,98 +37,100 @@ class MedicalItemCard extends StatelessWidget {
 
     double totalHeight = baseHeight + additionalHeight;
 
-    return FittedBox(
-      fit: BoxFit.fill,
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.5,
-        height: totalHeight,
-        padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 10.w),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.r),
-          border: Border.all(color: Color(0xFF555555), width: 0.5),
-          gradient: LinearGradient(
-            colors: [Color(0xFFECF5FF), Color(0xFFFBFDFF)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+    return GestureDetector(
+      onTap: () => onTap!(itemId),
+      child: FittedBox(
+        fit: BoxFit.fill,
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.5,
+          height: totalHeight,
+          padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 10.w),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16.r),
+            border: Border.all(color: Color(0xFF555555), width: 0.5),
+            gradient: LinearGradient(
+              colors: [Color(0xFFECF5FF), Color(0xFFFBFDFF)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 8.h, horizontal: isExpandingTitle ? 70.w : 6.w),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.r),
+                      border: Border.all(
+                        color: AppColorsManager.mainDarkBlue,
+                        width: 0.6,
+                      ),
+                    ),
+                    child: Text(
+                      title,
+                      maxLines: 1,
+                      style: AppTextStyles.font14BlueWeight700.copyWith(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16.sp,
+                        shadows: [
+                          Shadow(
+                            offset: Offset(1.5, 1.5),
+                            blurRadius: 3,
+                            color: Color(0x29000000),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+              verticalSpacing(4),
+              ...infoRows
+                  .map((row) => _infoRow(row["title"]!, row["value"]!))
+                  .toList(),
+              const Spacer(
+                flex: 4,
+              ),
+              InkWell(
+                borderRadius: BorderRadius.circular(16.r),
                 child: Container(
-                  padding: EdgeInsets.symmetric(
-                      vertical: 8.h, horizontal: isExpandingTitle ? 70.w : 6.w),
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16.r),
+                    color: Colors.transparent,
                     border: Border.all(
                       color: AppColorsManager.mainDarkBlue,
-                      width: 0.6,
+                      width: 1.6,
                     ),
+                    borderRadius: BorderRadius.circular(16.r),
                   ),
-                  child: Text(
-                    title,
-                    maxLines: 1,
-                    style: AppTextStyles.font14BlueWeight700.copyWith(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.sp,
-                      shadows: [
-                        Shadow(
-                          offset: Offset(1.5, 1.5),
-                          blurRadius: 3,
-                          color: Color(0x29000000),
-                        ),
-                      ],
-                    ),
-                    textAlign: TextAlign.center,
+                  padding: EdgeInsets.symmetric(vertical: 4.h),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "عرض المحتوى",
+                        style: AppTextStyles.font14BlueWeight700,
+                      ),
+                      horizontalSpacing(12),
+                      Image.asset(
+                        "assets/images/side_arrow_filled.png",
+                        width: 10.w,
+                        height: 10.h,
+                        color: AppColorsManager.mainDarkBlue,
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ),
-            verticalSpacing(4),
-            ...infoRows
-                .map((row) => _infoRow(row["title"]!, row["value"]!))
-                .toList(),
-            const Spacer(
-              flex: 4,
-            ),
-            InkWell(
-              onTap: () => onTap!(itemId),
-              borderRadius: BorderRadius.circular(16.r),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  border: Border.all(
-                    color: AppColorsManager.mainDarkBlue,
-                    width: 1.6,
-                  ),
-                  borderRadius: BorderRadius.circular(16.r),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 4.h),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "عرض المحتوى",
-                      style: AppTextStyles.font14BlueWeight700,
-                    ),
-                    horizontalSpacing(12),
-                    Image.asset(
-                      "assets/images/side_arrow_filled.png",
-                      width: 10.w,
-                      height: 10.h,
-                      color: AppColorsManager.mainDarkBlue,
-                    ),
-                  ],
-                ),
+              Spacer(
+                flex: 1,
               ),
-            ),
-            Spacer(
-              flex: 1,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
