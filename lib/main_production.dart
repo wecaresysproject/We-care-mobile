18831b2b0e76c98c76c1dccc729ea11e7aea08b5
kareem abdeen <kareem.elshreef18@gmail.com>
2025-04-17 16:34:25 +0200

    added launch .json , to have different configuration add dev and prod flavours config in build.gradle file change app_name in andriod manfiest file according to current running falvour added two main one for dev and one for prod

---
+++ lib/main_production.dart
@@ -0,0 +1,62 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:we_care/core/Database/cach_helper.dart';
+import 'package:we_care/core/di/dependency_injection.dart';
+import 'package:we_care/core/global/Helpers/extensions.dart';
+import 'package:we_care/core/networking/auth_api_constants.dart';
+import 'package:we_care/core/routing/app_router.dart';
+import 'package:we_care/features/emergency_complaints/data/models/medical_complaint_model.dart';
+import 'package:we_care/we_care_app.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setUpDependencyInjection();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  tz.initializeTimeZones();
+  await ScreenUtil.ensureScreenSize();
+
+  await checkIfLoggedInUser();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(MedicalComplaintAdapter());
+  await Hive.openBox<MedicalComplaint>("medical_complaints");
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // Light status bar
+      // statusBarIconBrightness: Brightness.dark, // Dark icons for status bar
+      systemNavigationBarColor: Colors.white, // Light navigation bar
+      // systemNavigationBarIconBrightness:
+      //     Brightness.dark, // Dark icons for navigation bar
+    ),
+  );
+  // Lock the app to portrait mode
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (_) {
+      runApp(
+        DevicePreview(
+          enabled: true,
+          builder: (context) => WeCareApp(
+            appRouter: AppRouter(),
+          ),
+        ),
+      );
+    },
+  );
+}
+
+Future<void> checkIfLoggedInUser() async {
+  String? userToken =
+      await CacheHelper.getSecuredString(AuthApiConstants.userTokenKey);
+  if (userToken.isEmptyOrNull) {
+    isLoggedInUser = false;
+  } else {
+    isLoggedInUser = true;
+  }
+}
