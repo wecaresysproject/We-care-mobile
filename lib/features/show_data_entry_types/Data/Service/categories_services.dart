f991aaf1b240df6a47736d6412e05151628dd884
Aya Nady <ayanady22193@gmail.com>
2025-04-23 19:22:23 +0200

    Adds categories repository and services; implements model for all categories tickets count and updates medical categories grid view to fetch data dynamically.

---
+++ lib/features/show_data_entry_types/Data/Service/categories_services.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'categories_services.g.dart';
+@RestApi(baseUrl: "http://147.93.57.70:5299/api/")
+abstract class CategoriesServices {
+  factory CategoriesServices(Dio dio, {String? baseUrl}) = _CategoriesServices;
+
+  @GET("Ticket/count")
+  Future<dynamic> getAllCategoriesTicketsCount(
+    @Query('language') String language,
+    @Query('userType') String userType,
+  );
+
+}
