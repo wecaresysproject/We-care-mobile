3728c99a4faa846381fe0821c366a56e2d07d622
Aya Nady <ayanady22193@gmail.com>
2025-06-22 17:12:17 +0300

    Enhance eye parts view with navigation and additional details

--- lib/features/eyes/eyes_view/Presentation/eye_part_procedures_and_symptoms_details_view.dart
+++ lib/features/eyes/eyes_view/Presentation/eye_part_procedures_and_symptoms_details_view.dart
@@ -4,6 +4,7 @@ import 'package:we_care/core/global/Helpers/functions.dart';
 import 'package:we_care/core/global/SharedWidgets/details_view_app_bar.dart';
 import 'package:we_care/core/global/theming/app_text_styles.dart';
 import 'package:we_care/core/global/theming/color_manager.dart';
+import 'package:we_care/features/eyes/eyes_view/Presentation/eye_parts_proscedure_and_symptoms_view.dart';
 import 'package:we_care/features/x_ray/x_ray_view/Presentation/views/widgets/x_ray_data_filters_row.dart';
 
 class EyePartProceduresAndSymptomsView extends StatelessWidget {
@@ -27,7 +28,8 @@ class EyePartProceduresAndSymptomsView extends StatelessWidget {
                   showActionButtons: false,
                 ),
                 DataViewFiltersRow(filters: [
-                  FilterConfig(title: '  السنة     ', options: ['2023', '2022']),
+                  FilterConfig(
+                      title: '  السنة     ', options: ['2023', '2022']),
                   FilterConfig(
                     title: '  الفئة      ',
                     options: [
@@ -36,27 +38,38 @@ class EyePartProceduresAndSymptomsView extends StatelessWidget {
                     ],
                   )
                 ], onApply: (selectedFilters) {}),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                return MedicalItemCardHorizontal(
-                  date: '1 / 3 / 2023',
-                  procedures: [
-                    'الاجراءات',
-                    'الاجراءات',
-                    'الاجراءات',
-                  ],
-                  symptoms: [
-                    'الاعراض',
-                    'الاعراض',
-                    'الاعراض',
-                  ],
-                );
-              }),
-              FooterRow(),
-              verticalSpacing(16),
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      return MedicalItemCardHorizontal(
+                        onArrowTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  EyePartsProcedureAndSymptomsDetailsView(
+                                title: eyePart,
+                              ),
+                            ),
+                          );
+                        },
+                        date: '1 / 3 / 2023',
+                        procedures: [
+                          'الاجراءات',
+                          'الاجراءات',
+                          'الاجراءات',
+                        ],
+                        symptoms: [
+                          'الاعراض',
+                          'الاعراض',
+                          'الاعراض',
+                        ],
+                      );
+                    }),
+                FooterRow(),
+                verticalSpacing(16),
               ],
             ),
           ),
@@ -80,163 +93,162 @@ class MedicalItemCardHorizontal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.h),
-      padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 10.w),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(18.r),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: const [Color(0xFFECF5FF), Color(0xFFFBFDFF)],
+    return InkWell(
+      onTap: onArrowTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.h),
+        padding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 10.w),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18.r),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: const [Color(0xFFECF5FF), Color(0xFFFBFDFF)],
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade200,
+              offset: const Offset(0, 1),
+              blurRadius: 3,
+            )
+          ],
+          border: Border.all(color: Colors.grey.shade400, width: 0.8),
         ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade200,
-            offset: const Offset(0, 1),
-            blurRadius: 3,
-          )
-        ],
-        border: Border.all(color: Colors.grey.shade400, width: 0.8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          /// Date Header
-          Center(
-            child: Container(
-              width: MediaQuery.of(context).size.width - 32.w,
-              padding: EdgeInsets.symmetric(vertical: 4.h, horizontal: 10.w),
-              decoration: BoxDecoration(
-                border: Border.all(color: AppColorsManager.mainDarkBlue),
-                borderRadius: BorderRadius.circular(12.r),
-              ),
-              child: Text(
-                date,
-                style:
-                    AppTextStyles.font14BlueWeight700.copyWith(fontSize: 14.sp),
-                textAlign: TextAlign.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Date Header
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 32.w,
+                padding: EdgeInsets.symmetric(vertical: 4.h, horizontal: 10.w),
+                decoration: BoxDecoration(
+                  border: Border.all(color: AppColorsManager.mainDarkBlue),
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                child: Text(
+                  date,
+                  style: AppTextStyles.font14BlueWeight700
+                      .copyWith(fontSize: 14.sp),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 10.h),
+            SizedBox(height: 10.h),
 
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  /// الإجراءات الطبية
-                  if (procedures != null && procedures!.isNotEmpty) ...[
-                    Text(
-                      'الإجراءات الطبية :',
-                      textAlign: TextAlign.start,
-                      style: AppTextStyles.font14BlueWeight700
-                          .copyWith(fontSize: 14.sp),
-                    ),
-                    ...procedures!.asMap().entries.map((entry) => Padding(
-                          padding: EdgeInsets.only(right: 12.w, top: 2.h),
-                          child: Text(
-                            '${entry.key + 1}/ ${entry.value}',
-                            style: AppTextStyles.font14blackWeight400,
-                          ),
-                        )),
-                    SizedBox(height: 8.h),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /// الإجراءات الطبية
+                    if (procedures != null && procedures!.isNotEmpty) ...[
+                      Text(
+                        'الإجراءات الطبية :',
+                        textAlign: TextAlign.start,
+                        style: AppTextStyles.font14BlueWeight700
+                            .copyWith(fontSize: 14.sp),
+                      ),
+                      ...procedures!.asMap().entries.map((entry) => Padding(
+                            padding: EdgeInsets.only(right: 12.w, top: 2.h),
+                            child: Text(
+                              '${entry.key + 1}/ ${entry.value}',
+                              style: AppTextStyles.font14blackWeight400,
+                            ),
+                          )),
+                      SizedBox(height: 8.h),
+                    ],
+
+                    /// الأعراض المرضية
+                    if (symptoms != null && symptoms!.isNotEmpty) ...[
+                      Text(
+                        'الأعراض المرضية :',
+                        style: AppTextStyles.font14BlueWeight700
+                            .copyWith(fontSize: 14.sp),
+                      ),
+                      ...symptoms!.asMap().entries.map((entry) => Padding(
+                            padding: EdgeInsets.only(right: 12.w, top: 2.h),
+                            child: Text(
+                              '${entry.key + 1}/ ${entry.value}',
+                              style: AppTextStyles.font14blackWeight400,
+                            ),
+                          )),
+                    ],
                   ],
-
-                  /// الأعراض المرضية
-                  if (symptoms != null && symptoms!.isNotEmpty) ...[
-                    Text(
-                      'الأعراض المرضية :',
-                      style: AppTextStyles.font14BlueWeight700
-                          .copyWith(fontSize: 14.sp),
-                    ),
-                    ...symptoms!.asMap().entries.map((entry) => Padding(
-                          padding: EdgeInsets.only(right: 12.w, top: 2.h),
-                          child: Text(
-                            '${entry.key + 1}/ ${entry.value}',
-                            style: AppTextStyles.font14blackWeight400,
-                          ),
-                        )),
-                  ],
-                ],
-              ),
-
-              const Spacer(),
-
-              /// Arrow Icon
-              Align(
-                alignment: Alignment.center,
-                child: IconButton(
-                  onPressed: onArrowTap,
-                  icon: Image.asset(
-                    'assets/images/side_arrow_filled.png',
-                    width: 24.w,
-                    height: 24.h,
-                  ),
                 ),
-              )
-            ],
-          )
-        ],
+
+                const Spacer(),
+
+                /// Arrow Icon
+                Align(
+                  alignment: Alignment.center,
+                  child: IconButton(
+                    onPressed: onArrowTap,
+                    icon: Image.asset(
+                      'assets/images/side_arrow_filled.png',
+                      width: 24.w,
+                      height: 24.h,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
 
 class FooterRow extends StatelessWidget {
   const FooterRow({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-  
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(158.w, 32.h),
-                    backgroundColor: AppColorsManager.mainDarkBlue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.r),
-                    ),
-                    padding: EdgeInsets.zero,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("عرض المزيد",
-                          style: AppTextStyles.font14whiteWeight600),
-                      horizontalSpacing(8.w),
-                      Icon(Icons.expand_more, color: Colors.white, size: 20.sp),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 47.w,
-                  height: 28.h,
-                  padding: EdgeInsets.symmetric(horizontal: 6.w),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(11.r),
-                    border: Border.all(
-                      color: AppColorsManager.mainDarkBlue,
-                      width: 2,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "+4",
-                      style: AppTextStyles.font16DarkGreyWeight400.copyWith(
-                        color: AppColorsManager.mainDarkBlue,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(158.w, 32.h),
+            backgroundColor: AppColorsManager.mainDarkBlue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.r),
+            ),
+            padding: EdgeInsets.zero,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("عرض المزيد", style: AppTextStyles.font14whiteWeight600),
+              horizontalSpacing(8.w),
+              Icon(Icons.expand_more, color: Colors.white, size: 20.sp),
+            ],
+          ),
+        ),
+        Container(
+          width: 47.w,
+          height: 28.h,
+          padding: EdgeInsets.symmetric(horizontal: 6.w),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(11.r),
+            border: Border.all(
+              color: AppColorsManager.mainDarkBlue,
+              width: 2,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              "+4",
+              style: AppTextStyles.font16DarkGreyWeight400.copyWith(
+                color: AppColorsManager.mainDarkBlue,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/features/eyes/eyes_view/Presentation/eye_parts_proscedure_and_symptoms_view.dart
+++ lib/features/eyes/eyes_view/Presentation/eye_parts_proscedure_and_symptoms_view.dart
@@ -1,39 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:we_care/core/global/SharedWidgets/details_view_app_bar.dart';
+import 'package:we_care/core/global/SharedWidgets/details_view_image_with_title.dart';
 import 'package:we_care/core/global/SharedWidgets/details_view_info_tile.dart';
 
 class EyePartsProcedureAndSymptomsDetailsView extends StatelessWidget {
-  const EyePartsProcedureAndSymptomsDetailsView({super.key, required this.title});
+  const EyePartsProcedureAndSymptomsDetailsView(
+      {super.key, required this.title});
   final String title;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          spacing: 16.h,
-          children: [
-            DetailsViewAppBar(title: title, showActionButtons: true),
-            DetailsViewInfoTile(
-              title: 'تاريخ الاعراض', value: '1 / 3 / 2025', icon: 'assets/images/date.png',
-              isExpanded: true ,
-              ),
-            DetailsViewInfoTile(
-              title: 'الاعراض', value: 'الم في العين', icon: 'assets/images/symptoms.png',
-              isExpanded: true ,
-              ),
-            DetailsViewInfoTile(
-              title: 'مدة الاعراض', value: '3 شهور', icon: 'assets/images/symptoms.png',
-              isExpanded: true ,
-              ),
-
-          ],
+        appBar: AppBar(
+          toolbarHeight: 0,
         ),
-      )
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              spacing: 16.h,
+              children: [
+                DetailsViewAppBar(title: title, showActionButtons: true),
+                DetailsViewInfoTile(
+                  title: 'تاريخ الاعراض',
+                  value: '1 / 3 / 2025',
+                  icon: 'assets/images/date_icon.png',
+                  isExpanded: true,
+                ),
+                DetailsViewInfoTile(
+                  title: 'الاعراض',
+                  value: 'الم في العين',
+                  icon: 'assets/images/symptoms_icon.png',
+                  isExpanded: true,
+                ),
+                DetailsViewInfoTile(
+                  title: 'مدة الاعراض',
+                  value: '3 شهور',
+                  icon: 'assets/images/symptoms_icon.png',
+                  isExpanded: true,
+                ),
+                DetailsViewInfoTile(
+                  title: 'تاريخ الاجراء الطبي',
+                  value: '1 / 3 / 2025',
+                  icon: 'assets/images/date_icon.png',
+                  isExpanded: true,
+                ),
+                DetailsViewInfoTile(
+                  title: 'الاجراء الطبي',
+                  value: 'الم في العين',
+                  icon: 'assets/images/doctor_icon.png',
+                  isExpanded: true,
+                ),
+                DetailsViewImageWithTitleTile(
+                  isShareEnabled: true,
+                  image: "https://www.researchgate.net/publication/291346521/figure/fig1/AS:614299588386842@1523471843460/Medical-Report-Form.png",
+                  title: 'الطبي التقرير',
+                ),
+                DetailsViewImageWithTitleTile(
+                  isShareEnabled: true,
+                  image: 'https://www.researchgate.net/publication/291346521/figure/fig1/AS:614299588386842@1523471843460/Medical-Report-Form.png',
+                  title: 'صورة الفحص الطبي',
+                ),
+                Row(
+                  children: [
+                    DetailsViewInfoTile(
+                      title: 'المستشفى',
+                      value: 'المستشفى',
+                      icon: 'assets/images/hospital_icon.png',
+                    ),
+                    Spacer(),
+                    DetailsViewInfoTile(
+                      title: 'الطبيب',
+                      value: 'د / احمد اسامة',
+                      icon: 'assets/images/doctor_name.png',
+                    ),
+                  ],
+                ),
+                DetailsViewInfoTile(
+                  title: 'الدولة',
+                  value: 'السعودية',
+                  icon: 'assets/images/country_icon.png',
+                ),
+                DetailsViewInfoTile(
+                  title: 'الملاحظات',
+                  value: 'لا يوجد',
+                  icon: 'assets/images/notes_icon.png',
+                  isExpanded: true,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
