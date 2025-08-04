88e6df8a4a9caa12ca95d0a01e59c84bfdaed533
kareem abdeen <kareem.elshreef18@gmail.com>
2025-08-04 20:06:00 +0300

    feat: update API endpoints to remove redundant prefixes and improve consistency

--- lib/features/surgeries/surgeries_api_constants.dart
+++ lib/features/surgeries/surgeries_api_constants.dart
@@ -1,29 +1,26 @@
-bool isLoggedInUser = false;
-
 class SurgeriesApiConstants {
-  static const baseUrl = "http://147.93.57.70:5299";
+  static const baseUrl = "http://147.93.57.70/api";
   //Data Entry
   // static const getCountries = "/api/EnterSectionSurgery/countries";
-  static const uploadReportEndpoint = "/m2/api/FileUpload/upload-report";
+  static const uploadReportEndpoint = "/FileUpload/upload-report";
   static const getAllSurgeriesRegions = // العضو
-      "/api/EnterSectionSurgery/surgery-regions";
+      "/EnterSectionSurgery/surgery-regions";
   static const getSubSurgeriesRegions =
-      "/api/EnterSectionSurgery/sub-surgery-regions-connected";
+      "/EnterSectionSurgery/sub-surgery-regions-connected";
 
-  static const getSurgeryName =
-      "/api/EnterSectionSurgery/surgery-names-connected";
+  static const getSurgeryName = "/EnterSectionSurgery/surgery-names-connected";
   static const getAllTechUsed =
-      "/api/EnterSectionSurgery/used-techniques-connected"; // التقنيه المستخدمه
+      "/EnterSectionSurgery/used-techniques-connected"; // التقنيه المستخدمه
   static const getSurgeryStatus =
-      "/api/EnterSectionSurgery/status-surgery"; // التقنيه المستخدمه
-  static const surgeryPurpose = "/api/EnterSectionSurgery/purpose";
-  static const postSurgeryEndpoint = "/api/EnterSectionSurgery";
+      "/EnterSectionSurgery/status-surgery"; // التقنيه المستخدمه
+  static const surgeryPurpose = "/EnterSectionSurgery/purpose";
+  static const postSurgeryEndpoint = "/EnterSectionSurgery";
   //View Entry
-  static const deleteSurgeryById = "/api/EnterSectionSurgery";
-  static const getAllSurgeries = "/api/EnterSectionSurgery";
-  static const getSingleSurgery = "/api/ViewSectionSurgery/filter-by-id";
-  static const editSurgeryEndpoint = "/api/EnterSectionSurgery";
+  static const deleteSurgeryById = "/EnterSectionSurgery";
+  static const getAllSurgeries = "/EnterSectionSurgery";
+  static const getSingleSurgery = "/ViewSectionSurgery/filter-by-id";
+  static const editSurgeryEndpoint = "/EnterSectionSurgery";
   //filters
-  static const getSurgeriesFilters = "/api/ViewSectionSurgery/surgery-info";
-  static const getFilteredSurgeries = "/api/ViewSectionSurgery/filter";
+  static const getSurgeriesFilters = "/ViewSectionSurgery/surgery-info";
+  static const getFilteredSurgeries = "/ViewSectionSurgery/filter";
 }
