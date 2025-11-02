852f6d89b92edce9a4def0c0c5d1b813e1407406
kareem abdeen <kareem.elshreef18@gmail.com>
2025-11-02 11:11:28 +0200

    remove static xray data view footer row widget

--- lib/features/x_ray/x_ray_view/Presentation/views/widgets/xray_list_bloc_builder.dart
+++ lib/features/x_ray/x_ray_view/Presentation/views/widgets/xray_list_bloc_builder.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:we_care/core/global/Helpers/app_enums.dart';
-import 'package:we_care/core/global/Helpers/functions.dart';
 import 'package:we_care/core/global/theming/app_text_styles.dart';
 import 'package:we_care/core/global/theming/color_manager.dart';
 import 'package:we_care/features/x_ray/x_ray_view/Presentation/views/widgets/xray_card_item_widget.dart';
@@ -70,63 +68,6 @@ class XrayListBlocBuilder extends StatelessWidget {
           return Container();
         }
       },
-    );
-  }
-}
-
-class XRayDataViewFooterRow extends StatelessWidget {
-  const XRayDataViewFooterRow({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            fixedSize: const Size(158, 32),
-            backgroundColor: AppColorsManager.mainDarkBlue,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.r),
-            ),
-            padding: EdgeInsets.zero,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "عرض المزيد",
-                style: AppTextStyles.font14whiteWeight600,
-              ),
-              horizontalSpacing(8),
-              Icon(
-                Icons.expand_more,
-                color: Colors.white,
-                weight: 100,
-                size: 24.sp,
-              ),
-            ],
-          ),
-        ),
-        Container(
-          width: 47.w,
-          height: 28.h,
-          padding: const EdgeInsets.symmetric(horizontal: 6),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(11.r),
-            border: Border.all(color: Color(0xFF014C8A), width: 2),
-          ),
-          child: Center(
-            child: Text(
-              "+10",
-              style: AppTextStyles.font16DarkGreyWeight400.copyWith(
-                color: AppColorsManager.mainDarkBlue,
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
