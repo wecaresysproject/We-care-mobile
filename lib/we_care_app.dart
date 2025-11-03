3f362a7fe4607192eaea7020775fe794c0c84734
Aya Nady <ayanady22193@gmail.com>
2025-11-04 01:20:33 +0200

    fix: uncomment initialRoute and comment out home in WeCareApp

--- lib/we_care_app.dart
+++ lib/we_care_app.dart
@@ -46,7 +46,7 @@ class WeCareApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: S.delegate.supportedLocales,
-      //    initialRoute: isLoggedInUser ? Routes.bottomNavBar : Routes.loginView,
+        initialRoute: isLoggedInUser ? Routes.bottomNavBar : Routes.loginView,
           theme: ThemeData(
             ///Later handle text field theme here to be same for all app
             //TODO: handle it later in seperate file
@@ -82,7 +82,7 @@ class WeCareApp extends StatelessWidget {
             ),
             useMaterial3: true,
           ),
-          home: const EssentialDataView(),
+       //   home: const EssentialDataView(),
         );
       },
     );
