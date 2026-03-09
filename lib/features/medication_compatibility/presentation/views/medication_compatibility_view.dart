1b47d0544e7ba53f4cfbe7ca132fafcaf59f93be
kareem abdeen <kareem.elshreef18@gmail.com>
2026-03-09 12:54:21 +0200

    feat: Replace the simple app bar with a new one supporting module guidance video and text in the medication compatibility view.

--- lib/features/medication_compatibility/presentation/views/medication_compatibility_view.dart
+++ lib/features/medication_compatibility/presentation/views/medication_compatibility_view.dart
@@ -3,9 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:we_care/core/di/dependency_injection.dart';
 import 'package:we_care/core/global/Helpers/functions.dart';
-import 'package:we_care/core/global/SharedWidgets/custom_app_bar_with_centered_title_widget.dart';
+import 'package:we_care/core/global/SharedWidgets/appbar_with_centered_title_with_guidance.dart';
+import 'package:we_care/core/global/SharedWidgets/module_guidance_alert_dialog.dart';
+import 'package:we_care/core/global/SharedWidgets/shared_app_bar_widget.dart';
+import 'package:we_care/core/global/theming/color_manager.dart';
 import 'package:we_care/features/medication_compatibility/presentation/views/widgets/medication_compitability_form_field_widget.dart';
 import 'package:we_care/features/medicine/medicines_data_entry/logic/cubit/medicines_data_entry_cubit.dart';
+import 'package:we_care/features/medicine/medicines_data_entry/logic/cubit/medicines_data_entry_state.dart';
 
 class MedicationCompatibilityView extends StatelessWidget {
   const MedicationCompatibilityView({super.key});
@@ -29,10 +33,54 @@ class MedicationCompatibilityView extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const AppBarWithCenteredTitle(
-                  title: 'اختبار توافق ادويتي',
-                  showActionButtons: false,
-                ),
+                BlocBuilder<MedicinesDataEntryCubit, MedicinesDataEntryState>(
+                    buildWhen: (previous, current) =>
+                        previous.moduleGuidanceData !=
+                        current.moduleGuidanceData,
+                    builder: (context, state) {
+                      return CustomAppBarWithCenteredTitleWithGuidance(
+                        title: 'اختبار توافق ادويتي',
+                        trailingActions: [
+                          CircleIconButton(
+                            size: 30.w,
+                            icon: Icons.play_arrow,
+                            color: state.moduleGuidanceData?.videoLink
+                                        ?.isNotEmpty ==
+                                    true
+                                ? AppColorsManager.mainDarkBlue
+                                : Colors.grey,
+                            onTap: state.moduleGuidanceData?.videoLink
+                                        ?.isNotEmpty ==
+                                    true
+                                ? () => launchYouTubeVideo(
+                                    state.moduleGuidanceData!.videoLink)
+                                : null,
+                          ),
+                          horizontalSpacing(8.w),
+                          CircleIconButton(
+                            size: 30.w,
+                            icon: Icons.menu_book_outlined,
+                            color: state.moduleGuidanceData?.moduleGuidanceText
+                                        ?.isNotEmpty ==
+                                    true
+                                ? AppColorsManager.mainDarkBlue
+                                : Colors.grey,
+                            onTap: state.moduleGuidanceData?.moduleGuidanceText
+                                        ?.isNotEmpty ==
+                                    true
+                                ? () {
+                                    ModuleGuidanceAlertDialog.show(
+                                      context,
+                                      title: "توافق ادويتي",
+                                      description: state.moduleGuidanceData!
+                                          .moduleGuidanceText!,
+                                    );
+                                  }
+                                : null,
+                          ),
+                        ],
+                      );
+                    }),
                 verticalSpacing(24),
                 MedicationCompatibilityFormFieldsWidget(),
               ],
