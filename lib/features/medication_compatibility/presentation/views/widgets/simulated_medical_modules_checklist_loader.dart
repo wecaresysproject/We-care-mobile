d296d59b5b1663caa7105cbed319e10ca6c0f0e2
kareem abdeen <kareem.elshreef18@gmail.com>
2026-03-16 12:11:49 +0200

    feat: Update HomeTabView navigation to directly push SimulatedMedicalModulesChecklistLoader instead of the medication compatibility named route.

--- lib/features/medication_compatibility/presentation/views/widgets/simulated_medical_modules_checklist_loader.dart
+++ lib/features/medication_compatibility/presentation/views/widgets/simulated_medical_modules_checklist_loader.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:we_care/core/global/theming/app_text_styles.dart';
 import 'package:we_care/core/global/theming/color_manager.dart';
-import 'package:we_care/features/medication_compatibility/presentation/views/medication_compatibility_result_view.dart';
-import 'package:we_care/features/medicine/medicines_data_entry/logic/cubit/medicines_data_entry_cubit.dart';
 
 class SimulatedMedicalModulesChecklistLoader extends StatefulWidget {
   const SimulatedMedicalModulesChecklistLoader({super.key});
@@ -48,23 +45,23 @@ class _SimulatedMedicalModulesChecklistLoaderState
   }
 
   void _startAnimation() {
-    _timer = Timer.periodic(const Duration(milliseconds: 250), (timer) {
+    _timer = Timer.periodic(const Duration(milliseconds: 350), (timer) {
       if (_currentIndex < _modules.length - 1) {
         setState(() {
           _currentIndex++;
         });
       } else {
         _timer?.cancel();
-        final cubit = context.read<MedicinesDataEntryCubit>();
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-              value: cubit,
-              child: const MedicationCompatibilityResultView(),
-            ),
-          ),
-        );
+        // final cubit = context.read<MedicinesDataEntryCubit>();
+        // Navigator.pushReplacement(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (_) => BlocProvider.value(
+        //       value: cubit,
+        //       child: const MedicationCompatibilityResultView(),
+        //     ),
+        //   ),
+        // );
       }
     });
   }
@@ -79,56 +76,71 @@ class _SimulatedMedicalModulesChecklistLoaderState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 0.h),
-      body: Column(
+      body: Stack(
         children: [
-          Text(
-            "جاري تجهيز ملفك الطبي",
-            style: AppTextStyles.font20blackWeight600.copyWith(
-              color: AppColorsManager.mainDarkBlue,
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Opacity(
+              opacity: 0.1,
+              child: Image.asset(
+                "assets/images/skeleton_image.png",
+                width: 300.w,
+                fit: BoxFit.contain,
+              ),
             ),
-            textAlign: TextAlign.center,
           ),
-          const SizedBox(height: 8),
-          Text(
-            "يتم الآن تحميل بياناتك الطبية وتحليلها",
-            style: AppTextStyles.font14blackWeight400.copyWith(
-              color: AppColorsManager.placeHolderColor,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 32),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _modules.length,
-              padding: EdgeInsets.symmetric(horizontal: 24.w),
-              itemBuilder: (context, index) {
-                bool isCompleted = index <= _currentIndex;
-                bool isProcessing = index == _currentIndex;
+          Column(
+            children: [
+              Text(
+                "جاري تجهيز ملفك الطبي",
+                style: AppTextStyles.font20blackWeight600.copyWith(
+                  color: AppColorsManager.mainDarkBlue,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "يتم الآن تحميل بياناتك الطبية وتحليلها",
+                style: AppTextStyles.font14blackWeight400.copyWith(
+                  color: AppColorsManager.placeHolderColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 32),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _modules.length,
+                  padding: EdgeInsets.symmetric(horizontal: 24.w),
+                  itemBuilder: (context, index) {
+                    bool isCompleted = index <= _currentIndex;
+                    bool isProcessing = index == _currentIndex;
 
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8.h),
-                  child: Row(
-                    children: [
-                      _buildIcon(isCompleted, isProcessing),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Text(
-                          _modules[index],
-                          style: AppTextStyles.font16BlackSemiBold.copyWith(
-                            color: isCompleted
-                                ? AppColorsManager.textColor
-                                : AppColorsManager.placeHolderColor,
-                            fontWeight: isCompleted
-                                ? FontWeight.bold
-                                : FontWeight.normal,
+                    return Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.h),
+                      child: Row(
+                        children: [
+                          _buildIcon(isCompleted, isProcessing),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Text(
+                              _modules[index],
+                              style: AppTextStyles.font16BlackSemiBold.copyWith(
+                                color: isCompleted
+                                    ? AppColorsManager.textColor
+                                    : AppColorsManager.placeHolderColor,
+                                fontWeight: isCompleted
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                              ),
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                );
-              },
-            ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ],
       ),
